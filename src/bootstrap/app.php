6a6b613d90e050ebfe8a9558b1ea2608401aaee2
Nadiia Astakhova <nadine.astakhova@gmail.com>
2026-01-19 16:13:16 +0100

    add admin middlwear app misisng

--- src/bootstrap/app.php
+++ src/bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Library\Infrastructure\Http\Middleware\AdminMiddleware;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -12,7 +13,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-
+        // Register custom middleware aliases
+        $middleware->alias([
+            'admin' => AdminMiddleware::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
